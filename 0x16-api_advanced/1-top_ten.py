a591b3105633b75c7ddf8b2724cdca9078f7b54a
lemejiamo <luismejia69@gmail.com>
2021-09-27 23:26:57 -0500

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module to testnig with Reddit API Educative project"""
+
+
+def top_ten(subreddit):
+    if subreddit is None:
+        return 0
+
+    import requests
+
+    headers = {'User-Agent': 'holbi/0.0.1'}
+    url = 'https://www.reddit.com/r/' + subreddit + '/hot/.json?count=10&limit=10'
+    response = requests.get(url, headers=headers, allow_redirects=False).json()
+
+    try:
+        for hot in response['data']['children']:
+            print(hot['data']['title'])
+    except:
+        return None
